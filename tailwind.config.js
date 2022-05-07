e8addee558e21c66da5673ca5d7de266125f1576
João Mota <joaommota.work@gmail.com>
2022-05-07 19:25:35 +0100

    feat(homepage): first implementation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'eul-banner': "url('/eul-banner.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
